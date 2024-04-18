48dc84026c547dc78eaafde2f5a852ba6cfb63cf
Francisco Alegre <franciscoalegre@gmail.com>
2024-04-18 18:07:18 -0300

    Agregando Ejercicio 1

---
+++ 02. java/Ejercicio1/untitled1/src/main/java/org/example/Ejercicio1.java
@@ -0,0 +1,13 @@
+package org.example;
+
+
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        String apellido = "Gomez";
+        int edad = 35;
+        boolean casado = false;
+        double sueldo = 45857.90;
+        String nombre = "Julián";
+    }
+}
