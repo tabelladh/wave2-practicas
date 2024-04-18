9fc5f1bdc72e6c8286f5a736c8599e3d838cc4f3
Francisco Alegre <franciscoalegre@gmail.com>
2024-04-18 18:13:02 -0300

    Agregando Ejercicio2

---
+++ 02. java/Clase - Java/Ejercicio2/src/main/java/org/example/Ejercicio2.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        String nombre1;
+        int edad;
+        double sueldo;
+        String apellido;
+        String direccion;
+        boolean licenciaDeConducir;
+        double estaturaDeLaPersona;
+        int cantidadDeHijos;
+
+    }
+}
+
