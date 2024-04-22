92967335c72b62783e7e87b489d5ba5a7516724b
ale seravalle <143661004+aleseravalle@users.noreply.github.com>
2024-04-22 15:11:47 -0300

    practica Clases - Profesora de Ingles

---
+++ 02. java/profesoraDeIngles/src/main/java/org/example/Alumno.java
@@ -0,0 +1,52 @@
+package org.example;
+
+public class Alumno {
+
+    int dni;
+    String nombre;
+    String apellido;
+    double promedio;
+
+    public Alumno() {
+
+    }
+
+    public Alumno(int dni, String nombre, String apellido, double promedio) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.promedio = promedio;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public double getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(double promedio) {
+        this.promedio = promedio;
+    }
+}
