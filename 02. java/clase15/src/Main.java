5f0d4d06689e87206af763494feb04dab030e7f3
abrilramos12 <abrilramos7@hotmail.com>
2024-04-24 11:21:49 -0300

    agregando ejercitación de la primera clase

---
+++ 02. java/clase15/src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+
+//Ejercicio 1
+       /* String apellido = "Gomez";
+        int edad = 35;
+        boolean miVariable = false;
+        double sueldo = 45857.90;
+        String nombre = "Julián";*/
+
+        //Ejecicio 2
+
+      /*  String nombre;
+        int edad;
+        double sueldo;
+        String apellido;
+        String direccion;
+        boolean licenciadeConducir;
+        double estaturaPersona;
+        int cantidaddeHijos; */
+
+        //Ejercicio 3
+
+        /*int dia = 2;
+        System.out.println("ingrese un numero");
+Scanner teclado = new Scanner(System.in);
+int num = teclado.nextInt();
+int resultado = dia*num;
+
+
+System.out.println("su monto es de" + " " + resultado);*/
+
+//Ejecicio 4
+
+        /*Scanner teclado = new Scanner(System.in);
+
+        System.out.println("ingrese el monto");
+        double num = teclado.nextInt();
+
+        double impuesto1 = num * 0.0245;
+        double impuesto2 = num  * 0.15;
+        double impuesto3 = num * 0.03;
+
+        double resultado = num - impuesto1 - impuesto2 - impuesto3;
+
+        System.out.println("su monto total es de:" + " " + resultado);*/
+
+
+
+    }
+}
