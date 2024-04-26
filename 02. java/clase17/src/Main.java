17a7539aee716672af189eb5733eb5fd4e5bd764
abrilramos12 <abrilramos7@hotmail.com>
2024-04-26 12:37:18 -0300

    correcciones de la clase 17, vectores y matrices

--- 02. java/clase17/src/Main.java
+++ 02. java/clase17/src/Main.java
@@ -4,36 +4,63 @@
 public class Main {
     public static <string> void main(String[] args) {
 
-    //MEJORAR
+        String[] ciudades = new String[10];
 
-        String[] ciudades = {"Londres", "Madrid", "Nueva York", "Buenos Aires", "Asunción", "São Paulo", "Lima", "Santiago de Chile", "Lisboa", "Tokio"};
-        double[][] temperatura = {
-                {-2, 33},
-                {-3,32},
-                {-8,27},
-                {4,37},
-                {6,42},
-                {0,39},
-                {-7,26},
-                {-1,31},
-                {-10,35}
-        };
+        ciudades[0] = "Londres";
+        ciudades[1] = "Madrid";
+        ciudades[2] = "Nueva York";
+        ciudades[3] = "Buenos Aires";
+        ciudades[4] = "Asunción";
+        ciudades[5] = "São Paulo";
+        ciudades[6] = "Lima";
+        ciudades[7] = "Santiago de Chile";
+        ciudades[8] = "Lisboa";
+        ciudades[9] = "Tokio";
 
-        for (int i = 0; i < ciudades.length; i++){
-            if(ciudades == "Tokio" ){
-                System.out.println("esta es la ciudad con menor temperatura:" + ciudades);
-            }else if(ciudades == "SaoPablo"){
-                System.out.println("esta es la ciudad con mayor temperatura:"  + ciudades);
+        double[][] temperatura = new double[10][2];
+
+        temperatura[0][0] = -2;
+        temperatura[0][1] = 33;
+        temperatura[1][0] = -3;
+        temperatura[1][1] = 32;
+        temperatura[2][0] = -8;
+        temperatura[2][1] = 27;
+        temperatura[3][0] = 4;
+        temperatura[3][1] = 37;
+        temperatura[4][0] = 6;
+        temperatura[4][1] = 42;
+        temperatura[5][0] = 0;
+        temperatura[5][1] = 39;
+        temperatura[6][0] = -7;
+        temperatura[6][1] = 26;
+        temperatura[7][0] = -1;
+        temperatura[7][1] = 31;
+        temperatura[8][0] = -10;
+        temperatura[8][1] = 35;
+        temperatura[9][0] = 1;  // Añadir valores para la fila 9
+        temperatura[9][1] = 2;  // Añadir valores para la fila 9
+
+        String ciudadTemperaturaMax = " ";
+        String ciudadTemperaturaMin = " ";
+
+        double temperaturaMax = 0;
+        double temperaturaMin = 0;
+
+        for (int i = 0; i < temperatura.length; i++) {
+            if (temperatura[i][0] > temperaturaMin ) {
+                temperaturaMin = temperatura[i][0];
+                ciudadTemperaturaMin = ciudades[i];
+
+            } if (temperatura[i][0] < temperaturaMax) {
+                temperaturaMax =temperatura[i][0];
+                ciudadTemperaturaMax = ciudades[i];
             }
+
         }
 
-        for (int i = 0; i < ciudades.length; i++){
-            if(temperatura == -10 ){
-                System.out.println("esta es la ciudad con menor temperatura:" + temperatura);
-            }else if(temperatura == 42){
-                System.out.println("esta es la ciudad con mayor temperatura:" + temperatura);
-            }
-        }
+        System.out.println("la temperatura minima es de " + temperaturaMin + " en la ciudad de:"+ ciudadTemperaturaMin);
+        System.out.println("la temperatura minima es de "+ temperaturaMax + " en la ciudad de:" + ciudadTemperaturaMax);
+
 
     }
 
