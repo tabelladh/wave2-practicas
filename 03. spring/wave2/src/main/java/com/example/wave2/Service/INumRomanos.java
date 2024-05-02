523565e8753bf96806819e50ba59914c821046c8
tabelladh <jtabella@digitalhouse.com>
2024-05-01 23:16:13 -0300

    Cambio de lugar la primer ejercitacion de Spring

---
+++ 03. spring/wave2/src/main/java/com/example/wave2/Service/INumRomanos.java
@@ -0,0 +1,7 @@
+package com.example.wave2.Service;
+
+public interface INumRomanos {
+
+    String transformarARomanos(Integer numDecimal);
+
+}
---
+++ 03. spring/wave2/src/test/java/com/example/wave2/Wave2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wave2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wave2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
