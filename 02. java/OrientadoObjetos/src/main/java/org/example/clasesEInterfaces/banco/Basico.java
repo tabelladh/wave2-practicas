b2b1fe85d2e6ecf546a30570f926e336940f2d54
Maca Ruffa <101282037+MacarenaRuffa@users.noreply.github.com>
2024-05-22 16:47:55 -0300

    ordenando

---
+++ 02. java/OrientadoObjetos/src/main/java/org/example/clasesEInterfaces/banco/Basico.java
@@ -0,0 +1,19 @@
+package basico;
+
+class Basico extends Cliente {
+
+    public Basico() {
+    }
+
+    public void consultaSaldo() {
+        System.out.println("Consultando el saldo.");
+    }
+
+    public void pagoServicios() {
+        System.out.println("Pagando servicios.");
+    }
+
+    public void retiroEfectivo() {
+        System.out.println("Retirando efectivo.");
+    }
+}
