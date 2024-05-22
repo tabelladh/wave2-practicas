28db60277bc058e705081a40731d09f04907b882
matiasoggero <matiassoggero@gmail.com>
2024-05-22 16:52:27 -0300

    add proyecto diploma

---
+++ 05. testing/ObtenerDiploma/src/main/java/com/digitalhouse/obtenerdiploma/exception/NotFoundAlumnoExeption.java
@@ -0,0 +1,5 @@
+package com.digitalhouse.obtenerdiploma.exception;
+
+public class NotFoundAlumnoExeption extends RuntimeException{
+
+}
---
+++ 05. testing/ObtenerDiploma/src/main/java/com/digitalhouse/obtenerdiploma/model/ErrorDTO.java
@@ -0,0 +1,18 @@
+package com.digitalhouse.obtenerdiploma.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDTO {
+
+    private String description;
+    @JsonProperty("messages_list")
+    private List<String> messagesList;
+}
