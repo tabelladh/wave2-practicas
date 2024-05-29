db95732f0c3a37ee481eecd1e7070ec052799df0
MartinBotta31 <martin.botta@sancorsalud.com.ar>
2024-05-29 18:59:11 -0300

    test unitario y validaciones

---
+++ 05. testing/ObtenerDiplomaTest/ObtenerDiploma/src/test/java/com/obtenerdiploma/util/repository/StudentRepositoryTest.java
@@ -0,0 +1,89 @@
+package com.obtenerdiploma.util.repository;
+
+import com.obtenerdiploma.entity.Student;
+import com.obtenerdiploma.entity.Subject;
+import com.obtenerdiploma.exception.StudentNotFoundException;
+import com.obtenerdiploma.repository.StudentRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+import java.util.Set;
+
+public class StudentRepositoryTest {
+
+    private static final Student student1 = new Student(1L,"Juan", Set.of(
+            new Subject("Matemática",9D),
+            new Subject("Física",7D),
+            new Subject("Química",6D)
+
+    ));
+
+    private static final Student student2 = new Student(2L,"Pedro", Set.of(
+            new Subject("Matemática",10D),
+            new Subject("Física",8D),
+            new Subject("Química",4D)
+
+    ));
+
+    StudentRepository repository = new StudentRepository(Set.of(student1,student2));
+
+
+    @Test
+    @DisplayName("Test01: findAll ok")
+    public void findAllTest(){
+        //ARRANGE
+        Set<Student> listaEsperada = Set.of(student1,student2);
+
+        //ACT
+        Set<Student> listaObtenida = repository.findAll();
+
+        //ASSERT
+        Assertions.assertEquals(listaEsperada,listaObtenida,"las listas no coinciden");
+
+
+    }
+
+    @Test
+    @DisplayName("02- Test findById ok ")
+    public void testFindByIdTestOK() {
+        //ARRANGE
+        Long idDeEntrada = 1L;
+        Optional<Student> studentEsperado = Optional.of(student1);
+
+        //ACT
+        Optional<Student> studentObtenido = repository.findById(idDeEntrada);
+
+        //ASSERT
+        Assertions.assertEquals(studentEsperado, studentObtenido, "No coinciden las listas");
+
+    }
+
+    @Test
+    @DisplayName("03- Test exists ok ")
+    public void testExistsTestOK() {
+        //ARRANGE
+        boolean studentesperadoSiExiste = true;
+
+        //ACT
+        boolean studentObtenido = repository.exists(student2);
+
+        //ASSERT
+        Assertions.assertEquals(studentesperadoSiExiste, studentObtenido, "No coinciden los students");
+    }
+
+    @Test
+    @DisplayName("04- Test no exists")
+    public void testExistsTestFAIL() {
+        //ARRANGE
+        Student studentDeEntrada = new Student(3L, "PEPE", Set.of(
+                new Subject("Matemática", 9D),
+                new Subject("Física", 7D),
+                new Subject("Química", 6D))
+        );
+
+        //ACT & ASSERT
+        Assertions.assertThrows(StudentNotFoundException.class, () -> repository.exists(studentDeEntrada));
+    }
+}
