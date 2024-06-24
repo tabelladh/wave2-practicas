c1fcd21316170c4a62a537985e3e1a8360a35a44
Matias-Oggero <matiassoggero@gmail.com>
2024-06-24 16:57:27 -0300

    agregamos practicas de implementacion

---
+++ 08. implementacion bd/miniSerie/src/test/java/com/example/miniSerie/MiniSerieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniSerie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniSerieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
